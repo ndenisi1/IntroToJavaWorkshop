6d031a30e6dfa9ffcd359425b00cdc438aa2ca78
league-workshopper-8 <workshop@MacBook-Pro.local>
2015-07-31 17:36:15 -0700

    puppy

--- src/day1/robot/StarShow.java
+++ src/day1/robot/StarShow.java
@@ -13,11 +13,10 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int wonderwoman = 
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
 
 		// 8. Make a variable to hold the star size and set it to 25
@@ -46,14 +45,16 @@ public class StarShow {
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+for (int i = 0; i < 5; i++) {
+	
 
 			// 1. Move the robot the distance of the starSize variable
-	
+robot.move(starSize);	
 			// 3. Turn the robot 144 degrees
-			
-	}
+	robot.turn(144);		
+	}}
 	
 	public static void main(String[] args) {
 		new StarShow().makeStars();
